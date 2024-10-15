0421344c00d1810de838aafb2e11f600a0235510
siennaamoils <sienna.amoils@student.uts.edu.au>
2024-10-16 10:59:38 +1100

    Added Instructions to identify port name on Mac and Windows

--- Lab2_Assessment/estopButton.m
+++ Lab2_Assessment/estopButton.m
@@ -1,4 +1,11 @@
 clear;
-arduinoBoard = arduino("/dev/tty.usbserial-2140", "Uno");
+
+% https://au.mathworks.com/help/matlab/supportpkg/find-arduino-port-on-windows-mac-and-linux.html
+% MATLAB Method: run "serialportlist" to identify USB port in Command Window
+% Windows Methoc: check Device Manager
+% Mac Method: "cd /" then "ls /dev/*" in Terminal
+
+arduinoBoard = arduino("/dev/tty.usbserial-2140", "Uno");   % mac
+%arduinoBoard = arduino("COM4", "Uno");                     % windows
 
 buttonState = readDigitalPin(arduinoBoard, 'D13');
