fe0bb9296a6a7f98593a7ba5c3db39f41bd964eb
fkout <fkout@DESKTOP-0H1VI7U>
2024-10-16 11:55:04 +1100

    reverted to original

--- Lab2_Assessment/DH_ABB.m
+++ Lab2_Assessment/DH_ABB.m
@@ -9,11 +9,14 @@ L6 = Link('d',	0.072,'a',0,'alpha',0,'qlim',deg2rad([-400,400]), 'offset', pi);
 robot = SerialLink([L1 L2 L3 L4 L5 L6],'name','IRB 120'); 
 
 q = zeros(1,robot.n); % This creates a vector of n joint angles at 0.
-workspace = [-4 +4 -4 +4 -4 +4];
+workspace = [-2 +2 -2 +2 -2 +2];
 scale = 2;
-q = [pi/2 pi/2 pi/2 pi/2 pi/2 pi/2];
+% q = [pi/2 pi/2 pi/2 pi/2 pi/2 pi/2];
 
 robot.plot(q,'workspace',workspace, 'scale', scale);
 
 % 1.3) Use teach to change the q variable (i.e. the values for each joint), and check that the model matches the images provided. 
 robot.teach(q);
+
+% r = DobotMagician;
+
