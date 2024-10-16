72a26cbe3aa4724ab1cd7454edf71d07b061d0a3
fkout <fkout@DESKTOP-0H1VI7U>
2024-10-16 11:14:28 +1100

    Changed qlim based on IRB documentation limits

--- Lab2_Assessment/DH_ABB.m
+++ Lab2_Assessment/DH_ABB.m
@@ -1,10 +1,10 @@
 %% DH parameters for the ABB IRB 120 robot
-L1 = Link('d',0.290,'a',0,'alpha',pi/2,'qlim',deg2rad([-270 270]), 'offset',0);
-L2 = Link('d',0,'a',0.270,'alpha',0,'qlim', deg2rad([-360 360]), 'offset',-pi/2);
-L3 = Link('d',0,'a',0.070,'alpha',-pi/2,'qlim', deg2rad([-360 360]), 'offset', 0);
-L4 = Link('d',0.302,'a',0,'alpha',pi/2,'qlim',deg2rad([-360 360]),'offset', 0);
-L5 = Link('d',0,'a',0,'alpha',-pi/2,'qlim',deg2rad([-360,360]), 'offset',0);
-L6 = Link('d',	0.072,'a',0,'alpha',0,'qlim',deg2rad([-360,360]), 'offset', pi);
+L1 = Link('d',0.290,'a',0,'alpha',pi/2,'qlim',deg2rad([-165 165]), 'offset',0);
+L2 = Link('d',0,'a',0.270,'alpha',0,'qlim', deg2rad([-110 110]), 'offset',-pi/2);
+L3 = Link('d',0,'a',0.070,'alpha',-pi/2,'qlim', deg2rad([-110 70]), 'offset', 0);
+L4 = Link('d',0.302,'a',0,'alpha',pi/2,'qlim',deg2rad([-160 160]),'offset', 0);
+L5 = Link('d',0,'a',0,'alpha',-pi/2,'qlim',deg2rad([-120,120]), 'offset',0);
+L6 = Link('d',	0.072,'a',0,'alpha',0,'qlim',deg2rad([-400,400]), 'offset', pi);
 
 robot = SerialLink([L1 L2 L3 L4 L5 L6],'name','IRB 120'); 
 
