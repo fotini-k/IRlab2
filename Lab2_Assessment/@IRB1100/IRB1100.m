e27122d2239912898232e50fb47137f30f16996f
philt <philt@Phillips-Laptop>
2024-10-24 21:49:31 +1100

    Ply file orientation changed

--- Lab2_Assessment/@IRB1100/IRB1100.m
+++ Lab2_Assessment/@IRB1100/IRB1100.m
@@ -45,8 +45,8 @@ classdef IRB1100 < RobotBaseClass
             % link(6) = Link('d',	0.072,'a',0,'alpha',0,'qlim',deg2rad([-400,400]), 'offset', pi);
 
             link(1) = Link('d',0.65,'a',0,'alpha',0,'qlim',deg2rad([-230 230]), 'offset',0);
-            link(2) = Link('d',1.9,'a',0,'alpha',pi/2,'qlim', deg2rad([-115 113]), 'offset', 0);
-            link(3) = Link('d',0 ,'a',0,'alpha',0,'qlim', deg2rad([-205 55]), 'offset', 0);
+            link(2) = Link('d',1.9,'a',0,'alpha',0,'qlim', deg2rad([-115 113]), 'offset', 0);
+            %link(3) = Link('d',0 ,'a',0,'alpha',0,'qlim', deg2rad([-205 55]), 'offset', 0);
             % link(4) = Link('d',0.2275,'a', 0.4,'alpha',0,'qlim',deg2rad([-230 230]),'offset', 0);
             % link(5) = Link('d',0 ,'a',1,'alpha',0,'qlim',deg2rad([-125,120]), 'offset',0);
             % link(6) = Link('d',	0,'a',0.35,'alpha',0,'qlim',deg2rad([-400,400]), 'offset', 0);
