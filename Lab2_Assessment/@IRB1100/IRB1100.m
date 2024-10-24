6cd1bb879143e6c8fc90068802e24849425e9a1f
philt <philt@Phillips-Laptop>
2024-10-24 20:00:37 +1100

    Corrected the DH parameters of the IRB1100 model

--- Lab2_Assessment/@IRB1100/IRB1100.m
+++ Lab2_Assessment/@IRB1100/IRB1100.m
@@ -44,14 +44,15 @@ classdef IRB1100 < RobotBaseClass
             % link(5) = Link('d',0,'a',0,'alpha',-pi/2,'qlim',deg2rad([-120,120]), 'offset',0);
             % link(6) = Link('d',	0.072,'a',0,'alpha',0,'qlim',deg2rad([-400,400]), 'offset', pi);
 
-            link(1) = Link('d',1,'a',0,'alpha',0,'qlim',deg2rad([-230 230]), 'offset',0);
-            % link(2) = Link('d',0,'a',0.290,'alpha',0,'qlim', deg2rad([-115 113]), 'offset', pi/2);
-            % link(3) = Link('d',0 ,'a',0,'alpha',-pi/2,'qlim', deg2rad([-205 55]), 'offset', 0);
-            % link(4) = Link('d',0.2275,'a', 0,'alpha',pi/2,'qlim',deg2rad([-230 230]),'offset', 0);
-            % link(5) = Link('d',0 ,'a',0,'alpha',-pi/2,'qlim',deg2rad([-125,120]), 'offset',0);
-            % link(6) = Link('d',	0.064,'a',0,'alpha',0,'qlim',deg2rad([-400,400]), 'offset', pi);
+            link(1) = Link('d',0.65,'a',0,'alpha',0,'qlim',deg2rad([-230 230]), 'offset',0);
+            link(2) = Link('d',0.75,'a',0,'alpha',0,'qlim', deg2rad([-115 113]), 'offset', 0);
+            link(3) = Link('d',1.3 ,'a',0,'alpha',0,'qlim', deg2rad([-205 55]), 'offset', 0);
+            link(4) = Link('d',0.2275,'a', 0.4,'alpha',0,'qlim',deg2rad([-230 230]),'offset', 0);
+            link(5) = Link('d',0 ,'a',1,'alpha',0,'qlim',deg2rad([-125,120]), 'offset',0);
+            link(6) = Link('d',	0,'a',0.35,'alpha',0,'qlim',deg2rad([-400,400]), 'offset', 0);
                    
             self.model = SerialLink(link,'name',self.name);  
+
 
             axis equal;
         
