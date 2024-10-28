4e29151bdb51aac0c7c51fbd7acef5c8faa356da
fkout <fkout@DESKTOP-0H1VI7U>
2024-10-28 13:31:14 +1100

    link 4 almost fixed

--- Lab2_Assessment/@IRB1100/IRB1100.m
+++ Lab2_Assessment/@IRB1100/IRB1100.m
@@ -47,7 +47,7 @@ classdef IRB1100 < RobotBaseClass
             link(1) = Link('d',1.65,'a',0,'alpha',pi/2,'qlim',deg2rad([-230 230]), 'offset',0);
             link(2) = Link('d',0,'a',1.5,'alpha',0,'qlim', deg2rad([-115 113]), 'offset', pi/2);
             link(3) = Link('d',0.1 ,'a',0,'alpha',pi/2,'qlim', deg2rad([-55 205]), 'offset', 0);
-             %link(4) = Link('d',0.2275,'a', 0.4,'alpha',0,'qlim',deg2rad([-230 230]),'offset', 0);
+            link(4) = Link('d',1.45,'a', 0,'alpha',0,'qlim',deg2rad([-230 230]),'offset', 0);
             % link(5) = Link('d',0 ,'a',1,'alpha',0,'qlim',deg2rad([-125,120]), 'offset',0);
             % link(6) = Link('d',	0,'a',0.35,'alpha',0,'qlim',deg2rad([-400,400]), 'offset', 0);
 
